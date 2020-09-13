7c1450b8766c579a35783b866ee18b7abed1abd0
SwaglordHabib <maunel.seelig@outlook.de>
2020-09-13 20:44:31 +0200

    removed Appsettings class and unused usings

--- Program.cs
+++ Program.cs
@@ -1,24 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Coravel;
-using Drunkenpolls.MangoMaracuja;
-using Microsoft.Extensions.Configuration;
+using Drunkenpolls.Bar;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Drunkenpolls.Zapfenstreich
 {
-    public class AppSettings
-    {
-        public string ConnectionString { get; set; }
-        public string mongodbDatabase { get; set; }
-        public string mongodbCollection { get; set; }
-
-    }
-
     public class Program
     {
         public static void Main(string[] args)
