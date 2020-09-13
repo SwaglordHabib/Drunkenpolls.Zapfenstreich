8dc2d0f03e4a3cf088ab65227703a8b7cb35e9e2
SwaglordHabib <maunel.seelig@outlook.de>
2020-09-13 20:45:34 +0200

    removed unused usings and obsolet classes that are now imported from github packages drunkenpolls.bar

--- Zapfenstreich.cs
+++ Zapfenstreich.cs
@@ -1,33 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Coravel.Invocable;
-using Drunkenpolls.MangoMaracuja;
-using Microsoft.Extensions.Configuration;
+using Drunkenpolls.Bar;
 using Microsoft.Extensions.Options;
-using MongoDB.Bson;
 using MongoDB.Driver;
-using MongoDB.Driver.Linq;
 
 namespace Drunkenpolls.Zapfenstreich
 {
-    public class Player
-    {
-        public string Name { get; set; }
-        public int Points { get; set; }
-        public string Role { get; set; }
-        public string connection { get; set; }
-    }
-    public class Game
-    {
-        public ObjectId _id { get; set; }
-        public string Pin { get; set; }
-        // 2020-06-11T22:55:07.784Z
-        public string Creation { get; set; }
-        public List<Player> Player { get; set; }
-    }
-
-
     public class Zapfenstreich : IInvocable
     {
         private readonly IDrunkenpollsDatabaseSettings _databaseSettings;
@@ -47,11 +26,12 @@ namespace Drunkenpolls.Zapfenstreich
             IMongoCollection<Game> collection = database.GetCollection<Game>(_databaseSettings.DrunkenpollsCollectionName);
 
             var gameToRemove = collection.Find(Builders<Game>.Filter.Empty).ToList();
-            gameToRemove = gameToRemove.FindAll(x => x.Creation != null ? DateTime.Parse(x.Creation).AddHours(1) < DateTime.Now : true);
+            gameToRemove = gameToRemove.FindAll(x => x.Creation == null
+                                                     || DateTime.Parse(x.Creation).AddHours(1) < DateTime.Now);
 
             foreach (var item in gameToRemove)
             {
-                collection.DeleteOne(Builders<Game>.Filter.Eq(x => x._id, item._id));
+                collection.DeleteOne(Builders<Game>.Filter.Eq(x => x.Id, item.Id));
             }
 
             Console.WriteLine(string.Format("{0} items have been removed.", gameToRemove.Count));
